d8afcb32ddee84c60a491d14d990165ddf232a52
Arkadiusz Konopka <arekcommerce@gmail.com>
2021-04-26 07:13:06 +0200

    added styles for mobile

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,27 +5,17 @@ const Coin = ({ key, name, symbol, currentPrice, marketCap, price_change_percent
 
   return (
     <div className='coin'>
-      <div className='coin-parameter'>
-        {/* <h4>Name</h4> */}
+      <div className="desktop">
         <p>{name}</p>
-      </div>
-      <div className='coin-parameter'>
-        {/* <h4>Symbol</h4> */}
         <p>{symbol}</p>
-      </div>
-      <div className='coin-parameter'>
-        {/* <h4>Current Price</h4> */}
         <p>{currentPrice} $</p>
-      </div>
-      <div className='coin-parameter'>
-        {/* <h4>Market Cap</h4> */}
         <p>{marketCap} $</p>
-      </div>
-      <div className='coin-parameter'>
-        {/* <h4 >24h Price</h4> */}
         <p style={{ color: price_change_percentage_24h > 0 ? "green" : "red" }}>{(price_change_percentage_24h * 10).toFixed(2)} %</p>
       </div>
-
+      <div className="mobile">
+        <p>{name}</p>
+        <p>{currentPrice} $</p>
+      </div>
     </div>
   )
 }
